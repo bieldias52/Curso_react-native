3f076be2792373e44acf18e054f4ea99e1bd5042
Gabriel Lange <104577517+bieldias52@users.noreply.github.com>
2022-05-04 11:26:26 -0300

    Add files via upload

---
+++ git_estudos/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from 'react-native'
+
+import Basico1 from "./src/Primeiro/Basico1";
+
+// export default () => <Basico1/>
+
+export default () => {
+  //console.warn('deu bom')
+  return (
+    <Basico1 />
+  )
+}
